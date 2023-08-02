1a2a0c58e537a2edd0d83a079f2fe6f145032160
Morshed Ahmed <mahmed162036@gmail.com>
2023-08-02 19:37:00 +0600

    Update footer.blade.php

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,6 +1,6 @@
     <!-- BEGIN: Footer-->
     <footer class="footer footer-static footer-light">
-        <p class="clearfix mb-0"><span class="float-left d-inline-block">2020 &copy; PIXINVENT</span><span class="float-right d-sm-inline-block d-none">Crafted with<i class="bx bxs-heart pink mx-50 font-small-3"></i>by<a class="text-uppercase" href="" target="_blank">Pixinvent</a></span>
+       <p class="clearfix mb-0"><span class="float-left d-inline-block">{{ date('Y') }} &copy; Zariq Ltd</span>
             <button class="btn btn-primary btn-icon scroll-top" type="button"><i class="bx bx-up-arrow-alt"></i></button>
         </p>
     </footer>
